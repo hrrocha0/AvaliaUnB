d390a509e220f23a924537a091233415b699e967
hrrocha0 <99548553+hrrocha0@users.noreply.github.com>
2023-07-08 12:02:36 -0300

    Added SQLite database

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
 val project_group: String by project
 val project_version: String by project
 val ktor_version: String by project
+val exposed_version: String by project
+val sqlite_jdbc_version: String by project
 val logback_version: String by project
 
 group = project_group
@@ -16,12 +18,17 @@ repositories {
 }
 
 dependencies {
+    // Ktor dependencies
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-server-freemarker:$ktor_version")
     implementation("io.ktor:ktor-server-auth:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-
+    // Exposed/SQLite dependencies
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.xerial:sqlite-jdbc:$sqlite_jdbc_version")
+    // Test dependencies
     testImplementation(kotlin("test"))
     testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
 }
--- src/main/kotlin/me/hrrocha0/avaliaunb/Main.kt
+++ src/main/kotlin/me/hrrocha0/avaliaunb/Main.kt
@@ -5,6 +5,7 @@ import io.ktor.server.netty.*
 import me.hrrocha0.avaliaunb.plugins.configureAuthentication
 import me.hrrocha0.avaliaunb.plugins.configureRouting
 import me.hrrocha0.avaliaunb.plugins.configureTemplating
+import org.jetbrains.exposed.sql.Database
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
@@ -14,5 +15,6 @@ fun Application.main() {
     configureRouting()
     configureTemplating()
 
+    Database.connect("jdbc:sqlite:main.sqlite", "org.sqlite.JDBC")
     log.info("Hello, World!")
 }
